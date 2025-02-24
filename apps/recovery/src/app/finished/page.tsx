130bbbecc8d55fd261f1bf990087af3e23cb21b2
joi.y.z <joi.yue.zou@gmail.com>
2025-02-24 15:26:38 -0800

    fix: recovery app build issue

--- apps/recovery/src/app/finished/page.tsx
+++ apps/recovery/src/app/finished/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Finished() {
   return (
-    <ContentWrapper title="Recovery Success" allSteps={3} currentStep={3}>
+    <ContentWrapper title="Recovery Success">
       <div className="flex flex-col items-center justify-center text-center gap-y-xl mx-20">
         <Image src={DoorImg} alt="door" width={164} height={164} />
         <p className="text-smaller text-gray-600">
