94e73c7ee017eeb9425d887f6785bc7cdaa11773
4bew <web4bew@gmail.com>
2025-03-08 08:16:16 +0800

    feat: typo

--- apps/extension/src/pages/LocalProfile.tsx
+++ apps/extension/src/pages/LocalProfile.tsx
@@ -4,7 +4,7 @@ import { SIDE_PANEL_ROUTE_PATHS } from '@/routes';
 
 export default function LocalProfile() {
   return (
-    <SecondaryPageWrapper title="Device Profile">
+    <SecondaryPageWrapper title="Device profile">
       <div className="space-y-2">
         <div
           className="elytro-rounded-border-item-wrapper hover:bg-gray-150"
