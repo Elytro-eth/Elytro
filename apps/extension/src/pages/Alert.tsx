161e68a635cf4d96f12a0e88dfeb4b60e21e6934
am9p <32947565+am9p@users.noreply.github.com>
2025-04-07 10:59:55 -0700

    fix: wrong display logic

--- apps/extension/src/pages/Alert.tsx
+++ apps/extension/src/pages/Alert.tsx
@@ -1,11 +1,10 @@
 import { useApproval } from '@/contexts/approval-context';
 import { ethErrors } from 'eth-rpc-errors';
 import Spin from '@/components/ui/Spin';
-import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import LogoWithCircle from '@/assets/error.svg';
-
+import { UnsupportedMethod } from '@/background/provider/rpcFlow/checkCallable';
+import ErrorIcon from '@/assets/error.svg';
 export default function BlockedAlert() {
   const { approval, reject } = useApproval();
 
@@ -22,36 +21,37 @@ export default function BlockedAlert() {
   };
 
   return (
-    <div className="w-full h-full bg-white flex justify-center items-center min-h-screen p-4">
-      <Card className="w-full h-full flex flex-col justify-between border-none rounded-none shadow-none">
-        <CardContent className="flex flex-col p-6 text-center">
-          <div className="flex justify-center mb-4">
-            <Avatar className="w-16 h-16 left-4 z-10 rounded-full">
-              <AvatarImage src={LogoWithCircle} alt={`${dApp.name} icon`} />
-              <AvatarFallback>{dApp.name}</AvatarFallback>
-            </Avatar>
-            <Avatar className="w-16 h-16 z-0 rounded-full mr-4">
-              <AvatarImage src={dApp.icon} alt={`${dApp.name} icon`} />
-              <AvatarFallback>{dApp.name}</AvatarFallback>
-            </Avatar>
+    <div className="flex flex-col gap-6 min-h-screen p-5xl bg-white text-center">
+      {/* DApp Info */}
+      <div className="flex items-center gap-4">
+        <Avatar className="size-12">
+          <AvatarImage src={dApp.icon} alt={`${dApp.name} icon`} />
+          <AvatarFallback>{dApp.name}</AvatarFallback>
+        </Avatar>
+        <div className="text-left">
+          <div className="elytro-text-body font-bold">{dApp.name}</div>
+          <div className="elytro-text-tiny-body text-gray-600">
+            {dApp.origin}
           </div>
-          <div className="mb-6">
-            <div className="elytro-text-body font-bold mb-2">
-              Connect to App
-            </div>
-            <div className="elytro-text-tiny-body text-gray-600">
-              {dApp.origin}
-            </div>
-          </div>
-          <div className="elytro-text-small">
-            Can’t connect to this app. <br />
-            {options?.reason || 'Unknown reason'}
-          </div>
-          <Button className="flex-1 mt-10" onClick={handleReject}>
-            OK
-          </Button>
-        </CardContent>
-      </Card>
+        </div>
+      </div>
+
+      <img src={ErrorIcon} alt="error" className="size-16 self-center" />
+
+      <div className="elytro-text-small">
+        The Dapp called
+        <span className="font-bold  px-xs">
+          {(options as UnsupportedMethod).name}
+        </span>
+        failed, the reason is
+        <span className=" font-bold  px-xs">
+          {(options as UnsupportedMethod).reason}
+        </span>
+      </div>
+
+      <Button className="w-full " onClick={handleReject}>
+        OK
+      </Button>
     </div>
   );
 }
