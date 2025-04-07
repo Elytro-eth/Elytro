aca14a85fe5463ad23f55d0b11b087ccc6df3034
4bew <web4bew@gmail.com>
2025-04-07 20:17:43 +0800

    feat: alert page add error icon

--- apps/extension/src/pages/Alert.tsx
+++ apps/extension/src/pages/Alert.tsx
@@ -4,6 +4,7 @@ import Spin from '@/components/ui/Spin';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import LogoWithCircle from '@/assets/error.svg';
 
 export default function BlockedAlert() {
   const { approval, reject } = useApproval();
@@ -24,8 +25,12 @@ export default function BlockedAlert() {
     <div className="w-full h-full bg-white flex justify-center items-center min-h-screen p-4">
       <Card className="w-full h-full flex flex-col justify-between border-none rounded-none shadow-none">
         <CardContent className="flex flex-col p-6 text-center">
-          <div className="flex justify-center mb-2">
-            <Avatar className="w-16 h-16 z-10 mt-3xl rounded-none">
+          <div className="flex justify-center mb-4">
+            <Avatar className="w-16 h-16 left-4 z-10 rounded-full">
+              <AvatarImage src={LogoWithCircle} alt={`${dApp.name} icon`} />
+              <AvatarFallback>{dApp.name}</AvatarFallback>
+            </Avatar>
+            <Avatar className="w-16 h-16 z-0 rounded-full mr-4">
               <AvatarImage src={dApp.icon} alt={`${dApp.name} icon`} />
               <AvatarFallback>{dApp.name}</AvatarFallback>
             </Avatar>
