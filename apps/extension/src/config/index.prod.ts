809af80a82f15d70089f4ea7da0af15b712da942
4bew <web4bew@gmail.com>
2025-04-24 10:59:37 +0800

    feat: support config in different evnorinment

---
+++ apps/extension/src/config/index.prod.ts
@@ -0,0 +1,15 @@
+export default {
+  graphqlEndpoint: 'https://api.soulwallet.io/elytroapi/graphql/',
+
+  firebaseVapidKey:
+    'BFL1Mk8n4N6DISyJSJ4h2MbIepF9Gg0OE5rcY2XMJU2D5ymSrLl-s4EdfsjgUw0f0xm2JqnxYbu2TnbV1fLRYxA',
+  firebaseConfig: {
+    apiKey: 'AIzaSyB6dpT2ILge1bCADdZjUC2FGoaRRLnrzuA',
+    authDomain: 'elytro-8de1d.firebaseapp.com',
+    projectId: 'elytro-8de1d',
+    storageBucket: 'elytro-8de1d.firebasestorage.app',
+    messagingSenderId: '877244822932',
+    appId: '1:877244822932:web:fa429dcac19ba0af290b81',
+    measurementId: 'G-6RLCJ33DJ8',
+  },
+};
