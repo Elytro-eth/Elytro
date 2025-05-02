2109ccce491d2015f4823c84272ff66c732d2d6a
4bew <web4bew@gmail.com>
2025-05-02 20:52:07 +0800

    fix: styles of title bar padding top

--- apps/extension/src/components/biz/SecondaryPageWrapper.tsx
+++ apps/extension/src/components/biz/SecondaryPageWrapper.tsx
@@ -40,22 +40,12 @@ export default function SecondaryPageWrapper({
 
   return (
     <div className={cn('w-full min-h-full bg-gray-150 p-sm', className)}>
-      <div className="flex flex-col flex-grow w-full min-h-full bg-white p-lg rounded-sm">
+      <div className="flex flex-col flex-grow w-full min-h-full bg-white p-lg pt-0 rounded-sm">
         {/* Header: back button, title, close button */}
         <div className="flex flex-row items-center justify-center relative py-lg mb-sm">
-          {showBack && (
-            <ArrowLeft
-              className="elytro-clickable-icon absolute left-0"
-              onClick={handleBack}
-            />
-          )}
+          {showBack && <ArrowLeft className="elytro-clickable-icon absolute left-0" onClick={handleBack} />}
           <h3 className="elytro-text-bold-body">{title}</h3>
-          {closeable && (
-            <X
-              className="elytro-clickable-icon absolute right-0"
-              onClick={handleClose}
-            />
-          )}
+          {closeable && <X className="elytro-clickable-icon absolute right-0" onClick={handleClose} />}
         </div>
 
         {children}
