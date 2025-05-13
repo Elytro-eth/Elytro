7f93e7c257a0d71707e89f7bdc9192d47aa3c110
5oqT <32947565+am9p@users.noreply.github.com>
2025-05-13 13:39:19 -0700

    Merge remote-tracking branch 'origin/style/update' into develop

--- apps/extension/src/components/biz/ContactItem.tsx
+++ apps/extension/src/components/biz/ContactItem.tsx
@@ -1,5 +1,5 @@
 import { formatAddressToShort } from '@/utils/format';
-import { UserRound } from 'lucide-react';
+import { CircleUserRound } from 'lucide-react';
 
 interface IContactItemProps {
   contact: TRecoveryContact;
@@ -7,17 +7,14 @@ interface IContactItemProps {
   rightContent?: React.ReactNode;
 }
 
-export default function ContactItem({
-  contact,
-  rightContent,
-}: IContactItemProps) {
+export default function ContactItem({ contact, rightContent }: IContactItemProps) {
   return (
     <div className="flex w-full items-center justify-between rounded-md border border-gray-300 bg-white p-lg">
       <div className="flex items-center gap-x-sm flex-1 min-w-0">
-        <UserRound className="size-2xl" />
+        <CircleUserRound className="size-2xl" />
 
         <div className="flex flex-col flex-1 min-w-0">
-          <p className="elytro-text-bold-body">{contact.name || '--'}</p>
+          <p className="elytro-text-bold-body">{contact.name || '—'}</p>
           <p
             className="elytro-text-tiny-body text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap"
             title={contact.address}
