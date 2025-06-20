a57f4d7f9adb755880be9f0794ce648ad001daa5
5oqT <32947565+5oqT@users.noreply.github.com>
2025-06-19 19:04:39 -0700

    fix: recovery build error

--- apps/recovery/src/app/layout.tsx
+++ apps/recovery/src/app/layout.tsx
@@ -1,16 +1,10 @@
-'use client';
-
-// import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './providers';
 import ConnectControl from '@/components/ConnectControl';
 import { LogoHeader } from '@/components/LogoHeader';
 import { Toaster } from '@/components/ui/toaster';
-import { getConfig } from '@/wagmi';
 import { Metadata } from 'next';
 import { usePathname } from 'next/navigation';
-import { cookieToInitialState } from 'wagmi';
-import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Elytro Recovery',
@@ -23,13 +17,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
-  const resolvedHeaders = await headers();
-  const initialState = cookieToInitialState(getConfig(), resolvedHeaders.get('cookie'));
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased w-screen h-screen screen-bg min-w-[800px]">
-        <Providers initialState={initialState}>
+        <Providers>
           <Toaster />
           <header className="min-w-[800px] fixed top-0 left-0 right-0 flex items-center justify-between gap-2 px-xl py-lg">
             <LogoHeader />
