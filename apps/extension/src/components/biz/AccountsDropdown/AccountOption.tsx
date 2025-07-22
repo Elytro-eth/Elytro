1ef2db3b8b2256cda2d398fd0014001dd5d6ffdc
5oqT <32947565+5oqT@users.noreply.github.com>
2025-07-21 20:33:53 -0700

    style: new delete confirm dialog

--- apps/extension/src/components/biz/AccountsDropdown/AccountOption.tsx
+++ apps/extension/src/components/biz/AccountsDropdown/AccountOption.tsx
@@ -1,10 +1,14 @@
-import { useAlert } from '@/components/ui/alerter';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Label } from '@/components/ui/label';
 import { getIconByChainId } from '@/constants/chains';
 import { toast } from '@/hooks/use-toast';
 import { formatAddressToShort, formatTokenAmount } from '@/utils/format';
 import { cn } from '@/utils/shadcn/utils';
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import { Trash2 } from 'lucide-react';
+import { useState } from 'react';
 
 interface IAccountOptionProps {
   account: TAccountInfo;
@@ -21,10 +25,13 @@ export default function AccountOption({
   onSelect,
   showDelete = true,
 }: IAccountOptionProps) {
-  const { elytroAlert } = useAlert();
+  const [isOpen, setIsOpen] = useState(false);
+  const [hasConfirmed, setHasConfirmed] = useState(false);
 
-  const handleDelete = (e: React.MouseEvent<SVGSVGElement>) => {
-    e.stopPropagation();
+  const handleDelete = () => {
+    if (!hasConfirmed) {
+      return;
+    }
 
     if (isSelected) {
       toast({
@@ -35,11 +42,7 @@ export default function AccountOption({
       return;
     }
 
-    elytroAlert({
-      title: 'Delete from this device',
-      description: 'The wallet can be recovered later.',
-      onConfirm: onDelete,
-    });
+    onDelete?.();
   };
 
   return (
@@ -62,11 +65,50 @@ export default function AccountOption({
       <div className="text-sm text-gray-600 flex flex-row items-center">
         <span className="text-gray-600">{formatTokenAmount(account?.balance, 18, 'ETH')}</span>
         {showDelete && !isSelected ? (
-          <Trash2 className="size-4 stroke-gray-600 hover:stroke-gray-900 ml-sm" onClick={handleDelete} />
+          <Trash2
+            className="size-4 stroke-gray-600 hover:stroke-gray-900 ml-sm cursor-pointer"
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsOpen(true);
+            }}
+          />
         ) : (
           <span></span>
         )}
       </div>
+
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="gap-y-lg max-w-md mx-auto" onClick={(e) => e.stopPropagation()}>
+          <DialogHeader>
+            <DialogTitle>Delete from this device</DialogTitle>
+          </DialogHeader>
+
+          <div className="flex flex-col gap-y-sm">
+            <div className="flex items-start gap-x-sm" onClick={() => setHasConfirmed((prev) => !prev)}>
+              <Checkbox checked={hasConfirmed} />
+              <Label className="elytro-text-small-body text-gray-600 cursor-pointer">
+                I understand I’ll lose access to this wallet unless I’ve backed up or enabled Social Recovery.
+              </Label>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-x-sm">
+            <Button variant="outline" className="w-full" onClick={() => setIsOpen(false)} size="small">
+              Cancel
+            </Button>
+
+            <Button
+              className="w-full"
+              onClick={handleDelete}
+              disabled={!hasConfirmed}
+              variant="destructive"
+              size="small"
+            >
+              CONFIRM DELETE
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
